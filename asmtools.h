2184a01147296c83caed94bc353959f3ec9f2ce0
Richard Allen <rsaxvc@gmail.com>
2020-10-02 01:09:58 -0500

    Add some nop-delay utilities

--- asmtools.h
+++ asmtools.h
@@ -11,4 +11,8 @@
 #define RIM() _asm("RIM") //Unblock interrupts
 #define WFI() _asm("WFI") //Wait for interrupt
 
+#define NOP4() NOP();NOP();NOP();NOP()
+#define NOP16() NOP4();NOP4();NOP4();NOP4()
+#define NOP64() NOP16();NOP16();NOP16();NOP16()
+#define NOP256() NOP64();NOP64();NOP64();NOP64()
 #endif
