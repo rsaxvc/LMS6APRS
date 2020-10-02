3837dfd5d0cd2a3bcaa683775da3a816ffe220ad
Richard Allen <rsaxvc@gmail.com>
2020-10-02 01:12:59 -0500

    Finish putchar
    Simulate putchar with the ST7 simulator
    and verify timing diagrams appear correct.

--- putchar.s
+++ putchar.s
@@ -2,12 +2,48 @@
 ;	Copyright (c) 2020 by SecKC
 ;
 
+;	switch	.ubsct
+;value: dc.b 1 ; fills .data section
+
 xdef	_putchar
 ;
 ;	putc routine to bit-bang an 8N1 software UART
 ;
 ;This algorithm is highly ST7-specific
 ;
+;This implementation relies on the following instruction timings:
+;    NOP2 - 2 cycles - Regular NOP
+;    NOP3 - 3 cycles - Custom slower NOP
+;    NOP5 - 5 cycles - Combo NOP
+;    NOP6 - 6 cycles - Combo NOP
+;    BRES - 5 cycles - Bit Reset
+;    BSET - 5 cycles - Bit Set
+;    BCP  - 2 cycles - Bit Compare
+;    JREQ - 3 cycles - Conditional Jump
+;    JRT  - 3 cycles - Jump
+;
+
+;Utility NOPs
+nop2: macro
+        nop
+	endm
+
+nop3: macro
+        jra \@macroret
+\@macroret:
+        endm
+
+nop5: macro
+        nop2
+	nop3
+        endm
+
+nop6: macro
+        nop3
+	nop3
+        endm
+
+
 ;SENDBIT MACROS
 ;All sendBit/sendStartBit/sendStopBit invocations must
 ;take the same number of cycles so the waveform stays
@@ -15,58 +51,62 @@ xdef	_putchar
 ;rate is determined by Fcpu / (number of cycles per bit)
 ;
 ;GPIO-write must always occur on the same cycle
-;(currently assumed to be instruction 3) to ensure
-;no jitter between bit-edges.
+;(currently assumed to start on cycle 6 of each
+;bit) to ensure no jitter between bit-edges.
 ;
-;This implementation assumes that each instruction takes
-;2 cycles, if this is not true the algorithm will need
-;to be adjusted once hardware is available. Of note,
-;the available documentation says instructions take
-;2-12 cycle, which is a pretty large spread.
+;This particular implementation implements 500kBaud
+;running on an 8MHz ST7.
 ;
-;This has not been tested.
-
 sendBit: macro ;ARGS: BankRegister, BankPin, BitNum
-        bcp     a, #\3
-        jreq    \@pos
-        bset    \1, #\2
-        jra     \@macroret
+        bcp     a, #\3      ;2 cycles
+        jreq    \@pos       ;3 cycles
+        bset    \1, #\2     ;5 cycles
+	nop3                ;3 cycles 
+        jra     \@macroret  ;3 cycles
 \@pos:
-        bres    \1, #\2
-        nop
+        bres    \1, #\2     ;5 cycles
+        nop6                ;6 cycles
 \@macroret:
 	endm
 
 sendStartBit: macro ;ARGS: BankRegister, BankPin
-	nop
-	nop
-	bres \1, #\2
-	nop
+	nop5                ;5 cycles
+	bres \1, #\2        ;5 cycles
+	nop6                ;6 cycles
 	endm
 sendStopBit: macro ;ARGS: BankRegister, BankPin
-	nop
-	nop
-	bset \1, #\2
-	nop
+	nop5                ;5 cycles
+	bset \1, #\2        ;5 cycles
+	nop6                ;6 cycles
 	endm
+
+#define PADR 0x0
+#define PBDR 0x3
+#define PCDR 0x6
+#define PDADR 0x9
+#define PEDR 0xC
+#define PFDR 0xF
+
+#define PORT_REG PBDR
+#define PIN 4
 
 	switch	.text
 
 _putchar:
 
-	ld      a, x             ;Load first COSMIC-C parameter into accumulator(it's passed in x)
-	sim                      ;Disable interrupts while we transmit a byte, this is why baud should be as high as possible
-	sendStartBit 0, 0        ;Drop GPIO low to send start bit
-	sendBit 0, 0, 0          ;Send Bit0
-	sendBit 0, 0, 1          ;Send Bit1
-	sendBit 0, 0, 2          ;Send Bit2
-	sendBit 0, 0, 3          ;Send Bit3
-	sendBit 0, 0, 4          ;Send Bit4
-	sendBit 0, 0, 5          ;Send Bit5
-	sendBit 0, 0, 6          ;Send Bit6
-	sendBit 0, 0, 7          ;Send Bit7
-	sendStopBit 0, 0	 ;Raise GPIO high to send end bit
-	rim                      ;Restore interrupts
+;	ld           value,a                   ;Load first COSMIC-C parameter into accumulator(it's passed in x)
+	sim                                    ;Disable interrupts while we transmit a byte, this is why baud should be as high as possible
+	sendStartBit PORT_REG, PIN             ;Drop GPIO low to send start bit
+	sendBit      PORT_REG, PIN, 0x01       ;Send Bit0
+	sendBit      PORT_REG, PIN, 0x02       ;Send Bit1
+	sendBit      PORT_REG, PIN, 0x04       ;Send Bit2
+	sendBit      PORT_REG, PIN, 0x08       ;Send Bit3
+	sendBit      PORT_REG, PIN, 0x10       ;Send Bit4
+	sendBit      PORT_REG, PIN, 0x20       ;Send Bit5
+	sendBit      PORT_REG, PIN, 0x40       ;Send Bit6
+	sendBit      PORT_REG, PIN, 0x80       ;Send Bit7
+	sendStopBit  PORT_REG, PIN             ;Raise GPIO high to send end bit
+	rim                                    ;Restore interrupts
 	ret
 
 	end
