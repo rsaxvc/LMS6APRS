72050e633f285fba63018fe29057d1a6cab42435
Richard Allen <rsaxvc@gmail.com>
2020-10-04 09:20:15 -0500

    Fix typo by bulk rename

--- Build.bat
+++ Build.bat
@@ -1,3 +1,3 @@
-cxst7 -l +modms +debug -itsip_parser cc1050.c crts.s delay.c putchar.s lsm6aprs.c init.c adc.c interrupt.c vector.c spilib.c tasks.c tsip_parser\tsip_parser.c
-clnk -o lsm6aprs.st7 lsm6aprs.lkf
-cvdwarf lsm6aprs.st7
+cxst7 -l +modms +debug -itsip_parser cc1050.c crts.s delay.c putchar.s lms6aprs.c init.c adc.c interrupt.c vector.c spilib.c tasks.c tsip_parser\tsip_parser.c
+clnk -o lms6aprs.st7 lms6aprs.lkf
+cvdwarf lms6aprs.st7
