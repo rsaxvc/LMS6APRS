c856e5e87d268c478db117fbf370f19a687e43c0
Richard Allen <rsaxvc@gmail.com>
2020-10-04 19:48:53 -0500

    Retime delay loops using simulator

--- delay.c
+++ delay.c
@@ -1,21 +1,26 @@
 #include "asmtools.h"
+#include "delay.h"
+//If the optimizer messes with these move to assembly.
 
-//
-void delay_millis( unsigned int ms )
+//This one is roughly correct
+void delay_millis( unsigned char ms )
 {
 while( ms )
 	{
-	delay_micros( 1000 );
+	//Adjust for the delay_micros() drift*/
+	delay_micros( 180 );
+	delay_micros( 180 );
+	delay_micros( 180 );
+	delay_micros( 180 );
 	ms--;
 	}
 }
 
-void delay_micros( unsigned int us )
+//This delay loop takes roughly 1.4us/us
+void delay_micros( unsigned char us )
 {
 while( us )
 	{
-	_asm("NOP");
-	_asm("NOP");
 	us--;
 	}
 }
---
+++ delay.h
@@ -0,0 +1,7 @@
+#ifndef DELAY_H
+#define DELAY_H
+
+void delay_millis( unsigned char ms );
+void delay_micros( unsigned char us );
+
+#endif
