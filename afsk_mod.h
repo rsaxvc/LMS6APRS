b7683c53d7a4172b4639e80bd74f93f0a1d48b41
Richard Allen <rsaxvc@gmail.com>
2020-10-09 17:17:59 -0500

    add a wavetable and refactor afsk modulator

---
+++ afsk_mod.h
@@ -0,0 +1,8 @@
+/*	Example Interrupt Handlers
+ *	Copyright (c) 2020 by SECKC Software
+ */
+#ifndef INTERRUPT_H
+#define INTERRUPT_H
+@interrupt void bad_interrupt(void);
+@interrupt void ei0_interrupt(void);
+#endif
--- interrupt.c
+++ interrupt.c
@@ -17,28 +17,3 @@
 	{
 	while(1);
 	}
-
-static const unsigned char dither_me_timbers[]=
-{
-0 , 1 , 1 , 1 , 1 , 0 , 1 , 1 , 1 , 1 , 0 , 1 , 1 , 0 , 1 , 0 , 1 , 0 , 1 , 0 , 1 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 1 , 0 , 1 , 0 , 1 , 0 , 1 , 0 , 1 , 1 , 0 , 1 , 1 , 1 , 1 , 0 , 1 , 1 , 1
-};
-
-unsigned char iterator;
-
-@interrupt void ei0_interrupt(void)
-{
-iterator++;
-if( iterator >= sizeof( dither_me_timbers ) )
-	{
-	iterator = 0;
-	}
-
-if( dither_me_timbers[iterator] )
-	{
-	GPIO_SET( GPIO_CC1050_DI_PORT, GPIO_CC1050_DI_PIN );
-	}
-else
-	{
-	GPIO_CLR( GPIO_CC1050_DI_PORT, GPIO_CC1050_DI_PIN );
-	}
-}
